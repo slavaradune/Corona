50ab50a1c61e9ab25d03333aed1dbb5e102475b4
Slava Radune <slava.radune@gmail.com>
2020-03-15 21:38:00 +0200

    Add logo and change menu color

--- src/Menu.js
+++ src/Menu.js
@@ -2,14 +2,15 @@ import React from 'react'
 import {Navbar, Nav} from 'react-bootstrap'
 import Language from './Language'
 import ReactFlagsSelect from 'react-flags-select';
+import logo from './ashkelon-logo.svg';
 
 class Menu extends React.Component {
     render() {
         let DICT = Language.getDict();
         let mrClass = (Language.getLanguage() === 'IL') ? 'ml-auto' : 'mr-auto';
         return(
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="/">{DICT.rules}</Navbar.Brand>
+            <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+                <Navbar.Brand href="/"><img src={logo} className="App-logo" alt="logo"/>{DICT.rules}</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className={mrClass}>

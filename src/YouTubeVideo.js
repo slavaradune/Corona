c038f2efc8975298fb8b12f064b54799e0739f59
Slava Radune <slava.radune@gmail.com>
2020-04-21 15:04:21 +0300

    allow video full screen

--- src/YouTubeVideo.js
+++ src/YouTubeVideo.js
@@ -1,29 +1,38 @@
 import React from 'react';
-import YouTube from 'react-youtube';
 
 class YouTubeVideo extends React.Component {
     render() {
-        const opts = {
-            height: 'auto',
-            width: 'auto',
-            // position: 'relative',
-            playerVars: {
-                autoplay: 0
+
+        let id = this.props.videoId;
+        if (id) {
+            if (id.includes('v=')){
+                id = this.props.videoId.split('v=')[1];
+                if (id.includes('&')) {
+                    id = id.split('&')[0];
+                }
+            } else if (id.includes('youtu.be/')) {
+                id = id.split('youtu.be/')[1];
             }
-        };
+        }
+
         return (
             <div
                 style={{textAlign: 'center'}}
             >
                 <div className='Video'>
                     <iframe
+                        allowfullscreen="allowfullscreen"
+                        mozallowfullscreen="mozallowfullscreen"
+                        msallowfullscreen="msallowfullscreen"
+                        oallowfullscreen="oallowfullscreen"
+                        webkitallowfullscreen="webkitallowfullscreen"
                         style={{
                             position: "relative",
                             top: 0,
                             width: "100%",
                             height: "100%"
                         }}
-                        src={`https://www.youtube.com/embed/${this.props.videoId}`}
+                        src={`https://www.youtube.com/embed/${id}`}
                         frameBorder="0"
                     />
                 </div>
